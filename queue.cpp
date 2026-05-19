d0a3a312a8a2113a93f07f91a7a62cc7b615e901
ihramfauzi22-maker <ihramfauzi22@gmail.com>
2026-05-19 14:45:14 +0700

    step2

---
+++ queue.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+using namespace std;
+
+class Queues
+{
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }    
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+
+        // Cek apakah antrian kosng
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            // jika REAR berada di posisi terakhir, maka kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+         queue_array[REAR] = num;
+    }
+   
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "\nQueue underflow\n";
+            return;
+        }
+
+        cout << "\nThe number deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // cek apakah antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // jika FRONT berada di posisi terakhir, maka kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;  
+    }
+}
+
+void display()
+    {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "\nQueue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        else
+        {
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid choice" << endl;
+                break;
+            }
+            }
+        }
+        catch(exception &e)
+        {
+            cout << "check for the values entered." << endl;
+        }
+        
+    }
+    return 0;
+}
