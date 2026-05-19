b090f301d3c011a3e3eecf2deb6c1fb8941607f1
ihramfauzi22-maker <ihramfauzi22@gmail.com>
2026-05-19 14:56:01 +0700

    step12

--- queue.cpp
+++ queue.cpp
@@ -103,4 +103,21 @@ void display()
             }
             cout << endl;
         }
-        
+        else
+        {
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
